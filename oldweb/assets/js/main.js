f80ac7540e8790d89443ca66e39b3c33c53924c4
LakshaySanhotra <lakshaysanhotra1@gmail.com>
2025-09-28 22:20:39 +0530

    portfolio change

---
+++ oldweb/assets/js/main.js
@@ -0,0 +1,44 @@
+AOS.init();
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 700, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+// heading type  JS
+let typed = new Typed(".auto-type", {
+  strings: [" DESIGNER", " DEVELOPER"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true
+});
+// mail send
+emailjs.init('6WHYOZSIeuLfoTwTh')
+const btn = document.getElementById('form_submit_btn');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_8arkajs';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
